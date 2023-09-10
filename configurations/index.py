cced4801ced3124a5dd3a16d4984901d81ac1eb4
Pranjal <epe18029@rgipt.ac.in>
2023-09-10 19:28:08 +0530

    config-changes

---
+++ configurations/index.py
@@ -0,0 +1,19 @@
+import configparser    
+    
+def read_section(section:str)->tuple:
+    status= False
+    try:
+        config = configparser.ConfigParser()
+        config.read(r'RealTimeSeries/configurations/config_file.ini')
+        config_sections  = config.sections()
+        if section not in config_sections:
+            return(status,"No such config found.")
+        config_set= {}
+        for key_config in config[section]:
+                config_set[key_config] = config[section][key_config]
+        status= True
+        return(status, config_set)
+    except Exception as e:
+        status= False
+        return (status,"Error message : {}.".format(str(e)))
+    
