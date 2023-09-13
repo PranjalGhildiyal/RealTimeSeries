4c60cbe01a83f2c268ae392dcea3511f08550ef2
Pranjal <epe18029@rgipt.ac.in>
2023-09-13 21:59:23 +0530

    url module finalized

--- src/Connections/url.py
+++ src/Connections/url.py
@@ -8,36 +8,37 @@ from RealTimeSeries.configurations.index import read_section
 class Connection:
     def __init__(self, url):
         self.url = url
-        self.is_valid = False  # Initialize a flag for valid connection
+        self.__connection_status = False  # Initialize a flag for valid connection
         status, logging_location= read_section('LOCATIONS')
         if not status:
             print('Could not find section. {}'.format(logging_location))
             return None
         global lg
         lg= Applogger(logging_location['url']).logger
-        self.__connection_status = 'Disconnected'
         self.__import_status = False
         self.data= None
 
         lg.info('Initiating Connection with Kafka.')
 
-    def test_connection(self):
+    def connect(self):
         try:
             # Send a HEAD request to the URL to check if it's reachable
             response = requests.head(self.url)
             
             # Check if the request was successful (status code 200 indicates success)
             if response.status_code == 200:
-                self.is_valid = True
+                self.__connection_status = True
                 lg.info('Connection test successful.')
             else:
                 lg.warning(f'Connection test failed. Status code: {response.status_code}')
         except Exception as e:
             lg.error(f'Error testing connection: {str(e)}')
 
-        return self.is_valid
-    
-    def download_data(self):
+        return self.__connection_status
+        
+
+    def get_schema(self):
+        status= False
         try:
             # Send an HTTP GET request to the URL
             response = requests.get(self.url)
@@ -45,23 +46,32 @@ class Connection:
             # Check if the request was successful (status code 200 indicates success)
             if response.status_code == 200:
                 # Get the content (data) from the response
-                self.data = response.content
-                lg.info('Data downloaded successfully.')
+                try:
+                    self.data = response.content
+                    data_str = self.data.decode('utf-8')
+                    self.data = pd.read_csv(io.StringIO(data_str))
+                    lg.info('Data downloaded successfully.')
+                    status= True
+                    return (status, list(self.data.columns))
+                except Exception as e:
+                    self.data = None
+                    lg.warning(f'Failed to convert data to readable dataframe.')
+                    return (status, f'Failed to convert data to readable dataframe.')
+
             else:
-                lg.warning(f'Failed to download data. Status code: {response.status_code}')
+                 lg.error(f'Failed to get data. Status code: {response.status_code}')
+                 return (status, f'Failed to get data. Status code: {response.status_code}')
+                 
         except Exception as e:
             lg.error(f'Error downloading data: {str(e)}')
+            return (status, f'Error downloading data: {str(e)}')
 
-    def get_data_as_dataframe(self):
+
+    def import_data(self):
+        status= False
         if self.data is not None:
-            try:
-                # Assuming the data is in CSV format, you can adjust this part as needed
-                data_str = self.data.decode('utf-8')
-                df = pd.read_csv(io.StringIO(data_str))
-                return df
-            except Exception as e:
-                lg.info(f'Error converting data to DataFrame: {str(e)}')
-                return None
+            status= True
+            return (status, self.data)
         else:
             lg.error('No data available to convert to DataFrame.')
-            return None
+            return (status, 'No data available to convert to DataFrame.')
