52c0d5f3fa00b6cce9ee297833b8b3f4b8a4c6d6
Pranjal <epe18029@rgipt.ac.in>
2023-09-16 17:04:42 +0530

    config-reader changes

---
+++ src/Components/ConfigReader.py
@@ -0,0 +1,14 @@
+from RealTimeSeries.configurations.index import read_section
+
+#===========================================================
+#                         Step 1
+#============================================================
+# Read all configs here.
+
+
+class ConfigReader:
+    def __init__(self):
+        print('in ConfigReader')
+        _, self.db_configs= read_section('SQLDB')
+        _, self.kafka_configs= read_section('KAFKA')
+        _, self.sharepoint_configs= read_section('SHAREPOINT')
