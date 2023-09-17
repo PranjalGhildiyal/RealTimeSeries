785a5b5b67fccbe361e0c3238f4e5273989540f9
Pranjal <epe18029@rgipt.ac.in>
2023-09-17 10:11:55 +0530

    added kafka stop widget

--- src/Components/WidgetDefinitions.py
+++ src/Components/WidgetDefinitions.py
@@ -23,12 +23,12 @@ class WidgetDefinitions(ConfigReader):
         self.get_schema_status= False
         self.import_data_status= False
 
-        self.last_n= pn.widgets.IntInput(name='Enter `n`', value=1, sizing_mode= 'stretch_width')
+        self.last_n= pn.widgets.IntInput(name='Enter `n` for metrics', value=1, sizing_mode= 'stretch_width')
         self.last_unit= pn.widgets.Select(name= 'Select unit', options= ['minutes', 'hours', 'seconds', 'days', 'months', 'years'], value='minutes', sizing_mode= 'stretch_width')
         self.n_indexes= pn.widgets.IntInput(name='Enter number of observations to display', sizing_mode= 'stretch_width', value= 100)
         self.feature_shape= pn.widgets.IntInput(name= 'Enter forecast shape', sizing_mode='stretch_width', value=20)
-        self.config_widgetbox= pn.WidgetBox(pn.Column(pn.Row(self.last_n, self.last_unit), pn.Row(self.feature_shape, self.n_indexes)), sizing_mode= 'stretch_width')
-
+        self.training_shape= pn.widgets.FloatInput(name='Enter training ratio', sizing_mode= 'stretch_width', value=0.8)
+        self.config_widgetbox= pn.WidgetBox(pn.Column(pn.Row(self.last_n, self.last_unit), pn.Row(self.feature_shape, self.n_indexes), self.training_shape), sizing_mode= 'stretch_width')
 
         # Making main
         self.gauge= pn.indicators.Gauge(name='Failure Rate', value=10, format= '{value}')
