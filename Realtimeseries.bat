7dbe963026d01c9930b6906e2f3475163baf70d6
Pranjal <epe18029@rgipt.ac.in>
2023-09-25 19:54:20 +0530

    bat file changes

--- Realtimeseries.bat
+++ Realtimeseries.bat
@@ -1,31 +1,33 @@
 @echo off
-cd %~dp0
+cd /d %~dp0
+set log_file=log.txt
 
-:: Set the name for the conda environment
-set conda_env_name=RealTimeSeries
+(
+    :: Set the name for the conda environment
+    set conda_env_name=RealTimeSeries
 
-:: Check if the conda environment exists
-conda env list | findstr /i "\<%conda_env_name%\>" > nul
-if %errorlevel% neq 0 (
-    :: Create a new conda environment with Python 3.8
-    conda create -n %conda_env_name% python=3.8 -y
+    :: Check if the conda environment exists
+    conda env list | findstr /i "\<%conda_env_name%\>" > nul
+    if %errorlevel% neq 0 (
+        :: Create a new conda environment with Python 3.8
+        conda create -n %conda_env_name% python=3.8 -y
+    ) > %log_file% 2>&1
+
+    :: Activate the conda environment
+    conda activate %conda_env_name% >> %log_file% 2>&1
+
+    :: Install dependencies from requirements.txt if not already installed
+    pip list | findstr /i /g:requirements.txt > nul
+    if %errorlevel% neq 0 (
+        pip install -r requirements.txt >> %log_file% 2>&1
+    )
+
+    :: Run your Python application (app.py)
+    python app.py >> %log_file% 2>&1
+
+    :: Exit the conda environment when done (optional)
+    conda deactivate >> %log_file% 2>&1
+
+    :: Display a completion message
+    echo Installation and execution complete. You can now close this window.
 )
-
-:: Activate the conda environment
-conda activate %conda_env_name%
-
-:: Install dependencies from requirements.txt if not already installed
-pip list | findstr /i /g:requirements.txt > nul
-if %errorlevel% neq 0 (
-    pip install -r requirements.txt
-)
-
-:: Run your Python application (app.py)
-cd %~dp0
-python app.py
-
-:: Exit the conda environment when done (optional)
-conda deactivate
-
-:: Display a completion message
-echo Installation and execution complete. You can now close this window.
