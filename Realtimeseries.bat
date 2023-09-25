6b7590d7783fbff82c75747bd0438af612692d3e
Pranjal <epe18029@rgipt.ac.in>
2023-09-25 20:57:28 +0530

    bat file changes (working noww)

--- Realtimeseries.bat
+++ Realtimeseries.bat
@@ -2,25 +2,25 @@
 cd /d %~dp0
 set log_file=log.txt
 
-(
-    :: Set the name for the conda environment
-    set conda_env_name=RealTimeSeries
+:: Set the name for the conda environment
+set conda_env_name=real5
 
+:: Activate the conda environment
+call conda activate %conda_env_name%
+
+(
     :: Check if the conda environment exists
     conda env list | findstr /i "\<%conda_env_name%\>" > nul
     if %errorlevel% neq 0 (
         :: Create a new conda environment with Python 3.8
-        conda create -n %conda_env_name% python=3.8 -y
-    ) > %log_file% 2>&1
+        conda create --name %conda_env_name% python=3.8 -y >> %log_file% 2>&1
+    )
 
-    :: Activate the conda environment
-    conda activate %conda_env_name% >> %log_file% 2>&1
+    :: Activate the conda environment again to ensure we are working within it
+    conda activate %conda_env_name%
 
     :: Install dependencies from requirements.txt if not already installed
-    pip list | findstr /i /g:requirements.txt > nul
-    if %errorlevel% neq 0 (
-        pip install -r requirements.txt >> %log_file% 2>&1
-    )
+    pip install -r requirements.txt >> %log_file% 2>&1
 
     :: Run your Python application (app.py)
     python app.py >> %log_file% 2>&1
@@ -31,3 +31,6 @@ set log_file=log.txt
     :: Display a completion message
     echo Installation and execution complete. You can now close this window.
 )
+
+:: Deactivate the conda environment if it's still active
+call conda deactivate >> %log_file% 2>&1
